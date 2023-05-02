ac0721599e068c31c5a135eda80d400ff1e35dd8
unknown <114369214+camfeitosa@users.noreply.github.com>
2023-05-02 12:27:21 -0300

    ajax

---
+++ 2bimestre/ajax/gethint.php
@@ -0,0 +1,34 @@
+<?php
+// array
+
+$a[] = "Lucifer";
+$a[] = "You";
+$a[] = "Friends";
+$a[] = "Euphoria";
+$a[] = "Loki";
+$a[] = "Alice in borderland";
+$a[] = "The Politician";
+$a[] = "How I Met Your Mother";
+
+$q = $_REQUEST["q"];
+
+$thint = "";
+
+if($q !== ""){
+    $q = strtolower($q);
+    $len=strlen($q);
+    foreach($a as $name){
+        if(stristr($q, substr($name, 0, $len))){
+            if($hint == ""){
+                $hint = $name;
+            } else {
+                $hint .= ", $name";
+                }
+            }
+        }
+    }
+
+
+
+    echo $thin == "" ? "Sem sugestão" : $hint;
+?>
