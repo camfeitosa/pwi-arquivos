2c2475d9565d5ffb6657c29021a9d55349f96729
camfeitosa~ <camilafeitosacoutinho@gmail.com>
2023-05-19 14:12:48 -0300

    upload

--- api/upload.php
+++ api/upload.php
@@ -1,6 +1,6 @@
 <?php
 if ($_FILES['file'] ['error'] === UPLOAD_ERR_OK ){
-    $uploadDir = 'imagens/';
+    $uploadDir = '/2bimestre/upload/imagens/';
     $uploadFile = $uploadDir . basename($_FILES['file'] ['name']);
 
     if (move_uploaded_file($_FILES['file'] ['tmp_name'], $uploadFile)){
