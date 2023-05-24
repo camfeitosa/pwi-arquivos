b01309fc4a24918a5d9f68bd623b09b6725a49e4
Camila Feitosa <114369214+camfeitosa@users.noreply.github.com>
2023-05-24 17:37:41 -0300

    edit

--- api/upload.php
+++ api/upload.php
@@ -1,6 +1,6 @@
 <?php
 if ($_FILES['file'] ['error'] === UPLOAD_ERR_OK ){
-    $uploadDir = '/2bimestre/upload/images/';
+    $uploadDir = 'images/';
     $uploadFile = $uploadDir . basename($_FILES['file'] ['name']);
 
     if (move_uploaded_file($_FILES['file'] ['tmp_name'], $uploadFile)){
