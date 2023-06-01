810426f063bc370d613c38c261d1b2842a0a2603
camfeitosa <camilafeitosacoutinho@gmail.com>
2023-06-01 12:27:54 -0300

    prova

---
+++ prova/ajax1.php
@@ -0,0 +1,27 @@
+<?php
+$arr_file_types = ['imagem/png', 'imagem/gif','imagem/jpg', 'imagem/jpeg'];
+
+//Verifica o tipo de arquivo
+
+if(! (in_array($_FILES['file'] ['type'], $arr_file_types))){
+    echo "error";
+    die;
+}
+
+//verifica o tamanho do arquivo
+
+$max_file_size = 1 * 1024 * 1024; //1 MB me bytes
+if ($_FILES['file'] ['size'] > $max_file_size ){
+    echo "";
+    die;
+}
+
+if  (!file_exists('uploads')){
+    mkdir('uploas', 0777);
+}
+$filename = time().'_'.$_FILES['file'] ['name'];
+
+//move_uploaded_file()
+
+
+?>
